e43bad47128709e931d4860d72cea0dd24f40726
Auy <apeetiwattanaphan@brandnewday.nl>
2021-10-18 13:43:26 +0700

    Init

---
+++ AzureLogging/Controllers/LoggingController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AzureLogging.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoggingController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Info")]
+        public IActionResult Info()
+        {
+            _logger.LogInformation("This is info log.");
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            try
+            {
+                throw new Exception("This is an exception.");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "This is error log.");
+            }
+
+            return Ok();
+        }
+    }
+}
