cc952e39f1a99146c98aafb5afeb7b0905afa3c1
Auy <apeetiwattanaphan@brandnewday.nl>
2021-10-18 13:55:14 +0700

    add serilog config

--- AzureLogging/Program.cs
+++ AzureLogging/Program.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
+using Serilog;
 
 namespace AzureLogging
 {
@@ -22,6 +17,20 @@ namespace AzureLogging
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.WithProperty("ApplicationName", "AzureLogging")
+                        .Enrich.WithProperty("Environment", "AuySpace")
+                        .Enrich.WithProperty("Logger", "Serilog")
+                        //.WriteTo.File(@"C:\logs\test\serilog.log")
+                        //.WriteTo.ApplicationInsights(new TelemetryConfiguration { InstrumentationKey = "0f08fccc-1618-409a-83d5-b4468fa4c21b" }, TelemetryConverter.Traces)
+                        //.WriteTo.File(
+                        //    @"D:\home\LogFiles\Application\trace.log",
+                        //    fileSizeLimitBytes: 5_000_000,
+                        //    rollOnFileSizeLimit: true,
+                        //    shared: true,
+                        //    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                );
     }
 }
